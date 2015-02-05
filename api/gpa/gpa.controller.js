d3053fb7683c79f7be71271f7c25995d644a5c5f
Rorschach22 <mcart046@morris.umn.edu>
2015-02-05 14:44:34 -0600

    Edited gpa.controller.js and index.js in api/gpa

--- api/gpa/gpa.controller.js
+++ api/gpa/gpa.controller.js
@@ -1,3 +1,54 @@
-/**
- * Created by mcart046 on 2/5/15.
- */
+'use strict';
+
+var mongoose = require('mongoose');
+
+// Defining Model
+// =====================================================
+
+var Gpa = mongoose.model('Gpa', {
+    class: String,
+    grade: String,
+    credits: String
+});
+
+// Defining Routes
+// =====================================================
+
+exports.index = function(req, res) {
+    Gpa.find(function (err, gpa) {
+        if (err) {
+            console.log("Error getting data from database");
+            res.send(err)
+        } else {
+            res.json(gpa); // return results
+        }
+    });
+};
+
+exports.create = function(req, res) {
+    Gpa.create(req.body, function (err, gpa) {
+        if (err) {
+            res.send(err);
+        } else {
+            Gpa.find(function (err, gpa) {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.json(gpa);
+            });
+        }
+    });
+};
+
+exports.destroy = function(req, res) {
+    Gpa.findById(req.params.gpa_id, function(err, gpa){
+        if(err) { res.send(err); return "error: " + err; }
+        if(!gpa) { return res.sendStatus(404); }
+
+        gpa.remove(function(err){
+            if(err) { return "error: " + err}
+            return res.sendStatus(204);
+        });
+    });
+};
--- api/gpa/index.js
+++ api/gpa/index.js
@@ -1,3 +1,16 @@
-/**
- * Created by mcart046 on 2/5/15.
- */
+'use strict';
+
+// Defining the "classes" API module
+// =======================================
+
+
+var express = require('express');
+var controller = require('./gpa.controller.js');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+router.post('/', controller.create);
+router.delete('/:gpa_id', controller.destroy);
+
+module.exports = router;
